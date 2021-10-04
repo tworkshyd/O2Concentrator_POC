d1efcb68484db255c11ddffcd0eb5afcb4a7a9c1
abdulWahab-tworks <abdul@tworks.in>
2021-10-04 10:50:09 +0530

    #if selection added for O2C concentration capping.

--- tworks_o2_cons/config.h
+++ tworks_o2_cons/config.h
@@ -20,12 +20,16 @@
 // 4. O2C cap-value setting
 #define O2C_CAP_VALUE_MAX   (95.0)
 
+// 5.
 // Select by uncommeting the required events from below list
 //  Note: these uncommented events print only if 'ENABLE_EVENT_LOGS' is enabled 
 #define CHANGE_IN_O2_LEVEL    
 #define CHANGE_IN_PRESSURE    
 #define CHANGE_IN_TEMPERATURE    
 
+
+// 6. O2c Capping enable / Disable selection
+#define CAPP_AT_95_O2 (1)
 
 
 
