63300d2e08b29b60db5d8887f2def04ff20c3b64
abdulWahab-tworks <abdul@tworks.in>
2021-10-05 14:53:04 +0530

    #if selections added for generating .hex files as per requirements.

--- tworks_o2_cons/config.h
+++ tworks_o2_cons/config.h
@@ -18,7 +18,7 @@
 #define ENABLE_DEBUG_PRINTS
 
 // 4. O2C cap-value setting
-#define O2C_CAP_VALUE_MAX   (95.0)
+#define O2C_CAP_VALUE_MAX           (95.0)
 
 // 5.
 // Select by uncommeting the required events from below list
@@ -28,19 +28,36 @@
 #define CHANGE_IN_TEMPERATURE    
 
 
-// 6. O2c Capping enable / Disable selection
-#define CAPP_AT_95_O2 (1)
+// 6. Select appropriate version for .hex file generation
+// Use appropriate selection below .. to generate .hex files .. as per the need..
+// Requirement..
+    //  M1 - v1.2 - no cap, 1st and 3rd relay reversal  --- O2 Sensor S1
+    //  M2 - v1.0 - with cap                            --- O2 Sensor S3
+    //  M3 - v1.1 - with cap                            --- O2 Sensor S2
 
-// 7. Relay selection for Z1 seive
-#define ENABLE_USE_OF_RELAY_3_FOR_Z1    (1)   // enabling this results in '1st and 3rd relay reversal'
+//#define FW_VERSION    (10)
+//#define FW_VERSION    (11)
+#define FW_VERSION      (12)
 
 
-// Use appropriate selection above .. to generate .hex files .. as per the need..
-//M3 - v1.1 - no cap                                --- O2 Sensor S3
-//M2 - v1.0 - with cap                              --- O2 Sensor S2
-//M1 - v1.2 - with cap, 1st and 3rd relay reversal  --- O2 Sensor S1
+#if (FW_VERSION == 10)
+    #define CAPP_AT_95_O2                   (1)   // O2c Capping enable / Disable selection
+    #define O2_SENSOR                       (3)   // O2 sensor selection among 3 
+    
+#elif (FW_VERSION == 11)
+    #define CAPP_AT_95_O2                   (1)   // O2c Capping enable / Disable selection
+    #define O2_SENSOR                       (2)   // O2 sensor selection among 3 
 
+#elif (FW_VERSION == 12)
+    #define CAPP_AT_95_O2                   (0)   // O2c Capping enable / Disable selection
+    #define O2_SENSOR                       (1)   // O2 sensor selection among 3 
+    #define ENABLE_USE_OF_RELAY_3_FOR_Z1    (1)   // enabling this results in '1st and 3rd relay reversal'
 
+#else
+    #define CAPP_AT_95_O2                   (1)   // O2c Capping enable / Disable selection
+    #define O2_SENSOR                       (3)   // O2 sensor selection among 3 
+
+#endif
 
 
 
